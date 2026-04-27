62617de01181ce38376840306cc445fcd96eb332
Leonardo Pires <leonardo.computei@gmail.com>
2026-04-27 15:24:45 -0300

    menu_usuarios
    foi acrescentado o menu de usuários do sistema

--- frontend/src/components/AppShell.jsx
+++ frontend/src/components/AppShell.jsx
@@ -29,6 +29,11 @@ const Icons = {
       <path d="M17 12h-5v5h5v-5zM16 1v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-1V1h-2zm3 18H5V8h14v11z" />
     </svg>
   ),
+  users: (
+    <svg viewBox="0 0 24 24" className="w-4 h-4 fill-current">
+      <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />
+    </svg>
+  ),
   logout: (
     <svg viewBox="0 0 24 24" className="w-4 h-4 fill-current">
       <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
@@ -60,7 +65,8 @@ const NAV_ITEMS = [
 ];
 
 const ADMIN_ITEMS = [
-  { to: '/admin', label: 'Gerenciar Rotas', icon: Icons.route },
+  { to: '/admin',    label: 'Gerenciar Rotas',    icon: Icons.route  },
+  { to: '/usuarios', label: 'Gerenciar Usuários', icon: Icons.users  },
 ];
 
 const ALL_NAV_ITEMS = [...NAV_ITEMS, ...ADMIN_ITEMS];
